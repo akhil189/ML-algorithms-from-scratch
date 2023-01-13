574aa42792691281a489cd91f29a9fa314c2c7c2
Akhil <saiakhilesh.a18@gmail.com>
2023-01-13 13:58:24 -0500

    renamed notebooks dir

--- ml_algs/svm_hard_margin.py
+++ ml_algs/svm_hard_margin.py
@@ -1,15 +1,42 @@
 import numpy as np
 
 class SVM():
+    """Implements hard-margin SVM(Support Vector Machines) Classification algorithm.
+    
+    Learns the parameters using Gradient Descent optimization
+    
+    Parameters
+    ----------
+    learningRate : The learning rate for gradient descent.
 
-    def __init__(self, learningRate = 0.0001, lamda = 0.001, nIters=1000):
+    lamda: The regularization parameter.
+
+    nIters: The maximum number of iterations for gradient descent.
+
+    Attributes
+    ----------
+    w : The weights of the SVM classifier.
+    
+    b : The bias of the SVM classifier.
+    """
+    def __init__(self, learningRate: float = 0.0001,
+                       lamda: float = 0.001,
+                       nIters: float = 1000):
+        """Inits the SVM class."""
 
         self.learningRate = learningRate
         self.lamda = lamda
         self.nIters = nIters
     
-    def fit(self, X, y):
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """Trains the SVM classifier using gradient descent.
         
+        Args:
+            X: The Data matrix
+
+            y: The target vector with class labels as +1 and -1.
+        """
+
         nSamples, nFeatures = X.shape
 
         self.w = np.zeros(nFeatures)
@@ -24,6 +51,15 @@ class SVM():
                     self.w = self.w - self.learningRate * (2 * self.lamda * self.w  - np.dot(x_i, y[idx]))
                     self.b = self.b - self.learningRate * (-y[idx])
     
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predicts the class labels for the given data.
+        
+        Args:
+            X: The Data matrix.
+
+        Returns:
+            The predicted class labels.
+        """
+
         pred_ = np.dot(self.w, X) + self.b
         return np.sign(pred_)
